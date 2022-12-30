892486ea909bf3feda1aab6572e7767dc1ad3b02
tuzlu07x <fatihtuzlu07@gmail.com>
2022-12-30 16:45:42 +0300

    employee request unique fixed.

--- app/Http/Requests/EmployeeRequest.php
+++ app/Http/Requests/EmployeeRequest.php
@@ -6,12 +6,18 @@ class EmployeeRequest extends BaseRequest
 {
     public function storeRules()
     {
-        //
+        return [
+            'social_security_number' => 'required|string|unique:employees,social_security_number',
+            'citizen_number' => 'required|string|unique:employees,citizen_number',
+        ];
     }
 
     public function updateRules()
     {
-        //
+        return [
+            'social_security_number' => 'required|string|unique:employees,social_security_number,' . $this->route('employee')->id,
+            'citizen_number' => 'required|string|unique:employees,citizen_number,' . $this->route('employee')->id,
+        ];
     }
 
     public function commonRules(): array
@@ -21,8 +27,7 @@ class EmployeeRequest extends BaseRequest
             'last_name' => 'required|string',
             'start_date' => 'required|date|after:yesterday',
             'end_date' => 'nullable|date|after_or_equal:start_date',
-            'social_security_number' => 'required|string|unique:employees',
-            'citizen_number' => 'required|string|unique:employees',
+
         ];
     }
 }
