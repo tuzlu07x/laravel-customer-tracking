6458be19ecfac2208105f31357a5fcdd9e68fb83
tuzlu07x <fatihtuzlu07@gmail.com>
2022-12-30 18:36:38 +0300

    search trait added for full name

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -5,13 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Traits\SearchTrait;
 
 class Employee extends Model
 {
     use HasFactory;
+    use SearchTrait;
+
     protected $fillable = ['first_name', 'last_name', 'start_date', 'end_date', 'social_security_number', 'citizen_number'];
     protected $appends = ['full_name'];
     protected $dates = ['start_date', 'end_date'];
+    protected $searchable = ['first_name', 'last_name'];
 
     public function leaves(): HasMany
     {
@@ -21,5 +25,17 @@ class Employee extends Model
     public function getFullNameAttribute()
     {
         return $this->first_name . ' ' . $this->last_name;
+    }
+
+    public function scopeSearch($query, $search)
+    {
+        $terms = $this->splitName($search);
+
+        foreach ($terms as $term) {
+            $query->where(function ($query) use ($term) {
+                $query->where('first_name', 'like', '%' . $term[0] . '%')
+                    ->orWhere('last_name', 'like', '%' . $term[1] . '%');
+            });
+        }
     }
 }
